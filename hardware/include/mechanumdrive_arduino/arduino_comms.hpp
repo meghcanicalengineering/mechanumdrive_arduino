a0ad07de73be3b591854f61cb386e7da8e23aff7
Meghana Fathepure <164543027+meghcanicalengineering@users.noreply.github.com>
2024-04-24 20:17:19 -0600

    Update arduino_comms.hpp

--- hardware/include/mechanumdrive_arduino/arduino_comms.hpp
+++ hardware/include/mechanumdrive_arduino/arduino_comms.hpp
@@ -1,118 +1,100 @@
-#ifndef DIFFDRIVE_ARDUINO_ARDUINO_COMMS_HPP
-#define DIFFDRIVE_ARDUINO_ARDUINO_COMMS_HPP
+#ifndef MECHANUMDRIVE_ARDUINO_ARDUINO_COMMS_HPP
+#define MECHANUMDRIVE_ARDUINO_ARDUINO_COMMS_HPP
 
-// #include <cstring>
 #include <sstream>
-// #include <cstdlib>
 #include <libserial/SerialPort.h>
 #include <iostream>
 
-
 LibSerial::BaudRate convert_baud_rate(int baud_rate)
 {
-  // Just handle some common baud rates
-  switch (baud_rate)
-  {
-    case 1200: return LibSerial::BaudRate::BAUD_1200;
-    case 1800: return LibSerial::BaudRate::BAUD_1800;
-    case 2400: return LibSerial::BaudRate::BAUD_2400;
-    case 4800: return LibSerial::BaudRate::BAUD_4800;
-    case 9600: return LibSerial::BaudRate::BAUD_9600;
-    case 19200: return LibSerial::BaudRate::BAUD_19200;
-    case 38400: return LibSerial::BaudRate::BAUD_38400;
-    case 57600: return LibSerial::BaudRate::BAUD_57600;
-    case 115200: return LibSerial::BaudRate::BAUD_115200;
-    case 230400: return LibSerial::BaudRate::BAUD_230400;
-    default:
-      std::cout << "Error! Baud rate " << baud_rate << " not supported! Default to 57600" << std::endl;
-      return LibSerial::BaudRate::BAUD_57600;
-  }
+    switch (baud_rate)
+    {
+        case 1200: return LibSerial::BaudRate::BAUD_1200;
+        case 1800: return LibSerial::BaudRate::BAUD_1800;
+        case 2400: return LibSerial::BaudRate::BAUD_2400;
+        case 4800: return LibSerial::BaudRate::BAUD_4800;
+        case 9600: return LibSerial::BaudRate::BAUD_9600;
+        case 19200: return LibSerial::BaudRate::BAUD_19200;
+        case 38400: return LibSerial::BaudRate::BAUD_38400;
+        case 57600: return LibSerial::BaudRate::BAUD_57600;
+        case 115200: return LibSerial::BaudRate::BAUD_115200;
+        case 230400: return LibSerial::BaudRate::BAUD_230400;
+        default:
+            std::cout << "Error! Baud rate " << baud_rate << " not supported! Default to 57600" << std::endl;
+            return LibSerial::BaudRate::BAUD_57600;
+    }
 }
 
 class ArduinoComms
 {
-
 public:
+    ArduinoComms() = default;
 
-  ArduinoComms() = default;
-
-  void connect(const std::string &serial_device, int32_t baud_rate, int32_t timeout_ms)
-  {  
-    timeout_ms_ = timeout_ms;
-    serial_conn_.Open(serial_device);
-    serial_conn_.SetBaudRate(convert_baud_rate(baud_rate));
-  }
-
-  void disconnect()
-  {
-    serial_conn_.Close();
-  }
-
-  bool connected() const
-  {
-    return serial_conn_.IsOpen();
-  }
-
-
-  std::string send_msg(const std::string &msg_to_send, bool print_output = false)
-  {
-    serial_conn_.FlushIOBuffers(); // Just in case
-    serial_conn_.Write(msg_to_send);
-
-    std::string response = "";
-    try
+    void connect(const std::string &serial_device, int32_t baud_rate, int32_t timeout_ms)
     {
-      // Responses end with \r\n so we will read up to (and including) the \n.
-      serial_conn_.ReadLine(response, '\n', timeout_ms_);
-    }
-    catch (const LibSerial::ReadTimeout&)
-    {
-        std::cerr << "The ReadByte() call has timed out." << std::endl ;
+        timeout_ms_ = timeout_ms;
+        serial_conn_.Open(serial_device);
+        serial_conn_.SetBaudRate(convert_baud_rate(baud_rate));
     }
 
-    if (print_output)
+    void disconnect()
     {
-      std::cout << "Sent: " << msg_to_send << " Recv: " << response << std::endl;
+        serial_conn_.Close();
     }
 
-    return response;
-  }
+    bool connected() const
+    {
+        return serial_conn_.IsOpen();
+    }
 
+    std::string send_msg(const std::string &msg_to_send, bool print_output = false)
+    {
+        serial_conn_.FlushIOBuffers(); // Clear buffers
+        serial_conn_.Write(msg_to_send);
 
-  void send_empty_msg()
-  {
-    std::string response = send_msg("\r");
-  }
+        std::string response = "";
+        try
+        {
+            serial_conn_.ReadLine(response, '\n', timeout_ms_);
+        }
+        catch (const LibSerial::ReadTimeout&)
+        {
+            std::cerr << "Read timeout." << std::endl;
+        }
 
-  void read_encoder_values(int &val_1, int &val_2)
-  {
-    std::string response = send_msg("e\r");
+        if (print_output)
+        {
+            std::cout << "Sent: " << msg_to_send << " Received: " << response << std::endl;
+        }
 
-    std::string delimiter = " ";
-    size_t del_pos = response.find(delimiter);
-    std::string token_1 = response.substr(0, del_pos);
-    std::string token_2 = response.substr(del_pos + delimiter.length());
+        return response;
+    }
 
-    val_1 = std::atoi(token_1.c_str());
-    val_2 = std::atoi(token_2.c_str());
-  }
-  void set_motor_values(int val_1, int val_2)
-  {
-    std::stringstream ss;
-    ss << "m " << val_1 << " " << val_2 << "\r";
-    send_msg(ss.str());
-  }
+    void read_encoder_values(int &fl_val, int &fr_val, int &rl_val, int &rr_val)
+    {
+        std::string response = send_msg("e\r");
 
-  void set_pid_values(int k_p, int k_d, int k_i, int k_o)
-  {
-    std::stringstream ss;
-    ss << "u " << k_p << ":" << k_d << ":" << k_i << ":" << k_o << "\r";
-    send_msg(ss.str());
-  }
+        std::istringstream iss(response);
+        iss >> fl_val >> fr_val >> rl_val >> rr_val;
+    }
+
+    void set_motor_values(int fl_val, int fr_val, int rl_val, int rr_val)
+    {
+        std::stringstream ss;
+        ss << "m " << fl_val << " " << fr_val << " " << rl_val << " " << rr_val << "\r";
+        send_msg(ss.str());
+    }
+
+    void set_pid_values(int k_p, int k_d, int k_i, int k_o)
+    {
+        std::stringstream ss;
+        ss << "p " << k_p << ":" << k_d << ":" << k_i << ":" << k_o << "\r";
+        send_msg(ss.str());
+    }
 
 private:
     LibSerial::SerialPort serial_conn_;
     int timeout_ms_;
 };
 
-#endif // DIFFDRIVE_ARDUINO_ARDUINO_COMMS_HPP
+#endif // MECHANUMDRIVE_ARDUINO_ARDUINO_COMMS_HPP
